0fca16c44a03527030f00a17e4ba42bf1a44d713
liuxin <liuxin@vn.com>
2025-07-30 11:23:20 +0800

    feat: ui

--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -5,7 +5,9 @@ import { ConfigProvider, Select } from 'antd';
 import { useState } from 'react';
 export default function Mint() {
   const [active, setActive] = useState<'buy' | 'sell'>('buy');
-  const [selectedCurrency, setSelectedCurrency] = useState<string>('USAD');
+  const [selectedCurrency, setSelectedCurrency] = useState<string>('GBPC');
+  const [selectedCurrency1, setSelectedCurrency1] = useState<string>('USAD');
+  const [selectedCurrency2, setSelectedCurrency2] = useState<string>('GBPC');
 
   return (
     <PageAnimate>
@@ -13,12 +15,12 @@ export default function Mint() {
         <div className="max-w-[1440px] m-auto flex justify-between ">
           <div>
             <AnimatedContent
-              content="充值本地法币购买加密货币"
+              content="Recharge local fiat currency to buy cryptocurrency"
               className="text-[32px] text-[#dac89f] leading-[48px] font-bold mb-[10px]"
               animateClassName="animate__slideInDown"
             ></AnimatedContent>
             <AnimatedContent
-              content="通过银行账户充值资金，然后用现金余额买 USAD"
+              content="Recharge funds through a bank account and then use the cash balance to buy USAD."
               className=" leading-6 text-sm text-[#FFFFFFA6]"
               animateClassName="animate__slideInUp"
             ></AnimatedContent>
@@ -53,6 +55,8 @@ export default function Mint() {
                 <div
                   onClick={() => {
                     setActive('buy');
+                    setSelectedCurrency('GBPC');
+                    setSelectedCurrency1('USAD');
                   }}
                   className={`p-7 text-center flex-1 text-[24px] font-[700] ${
                     active === 'buy' ? 'text-[#DAC89F]' : 'text-[#FFFFFF66]'
@@ -63,6 +67,8 @@ export default function Mint() {
                 <div
                   onClick={() => {
                     setActive('sell');
+                    setSelectedCurrency('USAD');
+                    setSelectedCurrency1('GBPC');
                   }}
                   className={`p-7 text-center flex-1 text-[24px] font-[700] ${
                     active === 'sell' ? 'text-[#DAC89F]' : 'text-[#FFFFFF66]'
@@ -73,7 +79,7 @@ export default function Mint() {
               </div>
               <div className=" rounded-[12px] bg-[#FFFFFF08] py-5 px-6">
                 <div className="text-[14px] text-[#FFFFFFD9] leading-[26px]">
-                  您将支付
+                  You will pay
                 </div>
                 <div className="flex mt-3 items-center justify-between">
                   <input
@@ -113,7 +119,7 @@ export default function Mint() {
               </div>
               <div className="mt-7 rounded-[12px] bg-[#FFFFFF08] py-5 px-6">
                 <div className="text-[14px] text-[#FFFFFFD9] leading-[26px]">
-                  您收到
+                  You receive
                 </div>
                 <div className="flex mt-3 items-center justify-between">
                   <input
@@ -122,18 +128,18 @@ export default function Mint() {
                   />
                   <Select
                     className="flex-1 h-[48px] flex items-center select_cc1 my-select"
-                    value={selectedCurrency}
+                    value={selectedCurrency1}
                     labelRender={() => (
                       <div className="flex items-center">
                         <div className="w-5 h-5 bg-[#1f8d09] rounded-full mr-2"></div>
-                        {selectedCurrency}
+                        {selectedCurrency1}
                       </div>
                     )}
                     getPopupContainer={() =>
                       document.querySelector('.select_cc1')
                     }
                     onChange={(value) => {
-                      setSelectedCurrency(value);
+                      setSelectedCurrency1(value);
                     }}
                     options={[
                       { value: 'USAD', label: 'USAD' },
@@ -154,23 +160,23 @@ export default function Mint() {
               <div className="mt-3 text-[#FFFFFF66] leading-[26px] text-sm">
                 1USDT ≈ 1.08 USD
               </div>
-              <div className="mt-8 text-white text-sm">支付方式</div>
+              <div className="mt-8 text-white text-sm">Pay</div>
               <Select
                 className="flex-1 mt-[10px] rounded-lg h-[48px] flex items-center border border-[#DAC89F1F] select_cc2 my-select"
-                value={selectedCurrency}
+                value={selectedCurrency2}
                 labelRender={() => (
                   <div className="flex items-center">
                     <div className="w-5 h-5 bg-[#1f8d09] rounded-full mr-2"></div>
-                    {selectedCurrency}
+                    {selectedCurrency2}
                   </div>
                 )}
                 getPopupContainer={() => document.querySelector('.select_cc2')}
                 onChange={(value) => {
-                  setSelectedCurrency(value);
+                  setSelectedCurrency2(value);
                 }}
                 options={[
-                  { value: 'USAD', label: 'USAD' },
-                  { value: 'USDT', label: 'USDT' },
+                  // { value: 'USAD', label: 'USAD' },
+                  // { value: 'USDT', label: 'USDT' },
                   { value: 'GBPC', label: 'GBPC' },
                 ].map((item) => ({
                   label: (
@@ -183,7 +189,7 @@ export default function Mint() {
                 }))}
               ></Select>
               <div className="mt-[45px] cursor-pointer mb-[32px] transition-all duration-300 active:bg-[#CCB47D] hover:bg-[#F1E3C1] rounded-lg h-[48px] justify-center font-bold text-[#000] bg-[#DAC89F] flex items-center">
-                Buy
+                {active === 'buy' ? 'Buy' : 'Sell'}
               </div>
             </div>
           </ConfigProvider>
